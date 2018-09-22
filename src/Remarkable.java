487bb9d488641252462e3fd70df057bc81e266e2
Kendra Kleber <league@Mac-mini-2.local>
2018-09-22 11:22:56 -0700

    unfinished Remakebl

--- src/Remarkable.java
+++ src/Remarkable.java
@@ -4,11 +4,12 @@ public class Remarkable {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String name = JOptionPane.showInputDialog(null, "Whats your name?");
-if(name.equalsIgnoreCase("julia")){
-	JOptionPane.showMessageDialog(null, "Hi " + name + ". Your Remarkable");
-	 
-}
+		String name = JOptionPane.showInputDialog(null, "What's your name?");
+		if (name.equalsIgnoreCase("julia")) {
+			JOptionPane.showMessageDialog(null, "Hi " + name + ". You're remarkable because you're going to NASA.");
+		} else if (name.equalsIgnoreCase("ronald")) {
+			JOptionPane.showMessageDialog(null, "Hi "+ name + "You're remarkable because you're the youngest stundent in the league  .");
+		}
 	}
 
 }
