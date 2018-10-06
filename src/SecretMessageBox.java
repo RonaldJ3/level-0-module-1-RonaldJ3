b46b02e7d7c423469860775298157ab2ecc74c43
RonaldJ3 <league@WTS-MM-7.local>
2018-10-06 11:24:19 -0700

    I finished secret message box and started the Riddler

--- src/SecretMessageBox.java
+++ src/SecretMessageBox.java
@@ -4,9 +4,17 @@ public class SecretMessageBox {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String password ="ron";
-		String message = JOptionPane.showInputDialog(null, "Hi please type a secret message.");
-	String Input =	JOptionPane.showInputDialog(null, "you can only see the secret message if you you can guess the passcode");
+		String password = "zone";
+		String message = JOptionPane.showInputDialog("Hi. Please type a secret message.");
+
+		String Input = JOptionPane
+				.showInputDialog("You can only see the secret message if you you can guess the passcode");
+
+		if (Input.equalsIgnoreCase(password)) {
+			JOptionPane.showMessageDialog(null, message);
+		} else {
+			JOptionPane.showMessageDialog(null, "INTRUDER!!");
+		}
 	}
 
 }
---
+++ src/TheRiddler.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class TheRiddler {
+public static void main(String[] args){
+ JOptionPane.showInputDialog("I am always hungry, I must always be fed The finger I touch, will soon be red");
+ 
+}
+}
