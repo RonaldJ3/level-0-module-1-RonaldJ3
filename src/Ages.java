57f13b09370f931fc125d770c49661cded8c6d85
Kendra Kleber <league@Mac-mini-2.local>
2018-11-03 11:26:16 -0700

    I completed ages and started Badger

--- src/Ages.java
+++ src/Ages.java
@@ -2,5 +2,8 @@ import javax.swing.JOptionPane;
 
 public class Ages {
 	public static void main(String[] args) {
+for(int i=1;i < 11; i= i+ 1 ) {
+	System.out.println(i);
 }
+	}
 }
---
+++ src/Badger.java
@@ -0,0 +1,18 @@
+
+public class Badger {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "badger, ";
+		String fun = "Mushroom , ";
+		for(int i=1;i < 11; i= i+ 1 ) {
+			System.out.print(name);
+		}
+		System.out.println();
+		for(int i=1;i < 3; i= i+ 1 ) {
+			System.out.print(fun a);
+		}
+
+		}
+
+}
