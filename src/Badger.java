68537a25c9122a126aceda1c8b8607de8a21bef5
RonaldJ3 <league@WorthyMini.local>
2018-11-17 11:26:37 -0800

    I continued badger

--- src/Badger.java
+++ src/Badger.java
@@ -5,78 +5,33 @@ public class Badger {
 		// TODO Auto-generated method stub
 		String name = "badger, ";
 		String fun = "Mushroom , ";
-		for(int i=1;i < 11; i= i+ 1 ) {
+		String boop = "Snake ,";
+		for (int i = 1; i < 11; i = i + 1) {
 			System.out.print(name);
+		}
+		for (int m = 0; m < 2; m++) {
+			System.out.println();
+			for (int i = 1; i < 3; i = i + 1) {
+				System.out.print(fun);
+				System.out.print(fun.toLowerCase());
+				System.out.print("a");
+				for (int j = 1; i < 11; i = i + 1) {
+					System.out.println();
+					for (int k = 1; i < 11; i = i + 1){
+						System.out.print(name);
+					}
+				}
+			}
 		}
 		System.out.println();
-		for(int i=1;i < 3; i= i+1 ) {
-			System.out.print(fun);
+		System.out.print("Mush-");
+		for (int i = 1; i < 2; i = i + 1) {
 			System.out.print(fun.toLowerCase());
-				System.out.print("a");
-			for(int j=1;i < 11; i= i+ 1 ) {
-				System.out.println();
-			for(int k=1;i < 11; i= i+ 1 ) 
-				System.out.print(name);
-		}for(int j=1;i < 11; i= i+ 1 ) {
-			System.out.print(name);
-			for(int k=1;i < 11; i= i+ 1 );
-		}
-		}
-	}	}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+			System.out.println("a");
+		for (int k = 1; i < 11; i = i + 1){
+			System.out.println(name);
+			System.out.println(boop.toLowerCase());
+	}
+}
+}
+}
